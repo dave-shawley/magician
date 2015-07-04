3234a37260de928a209adffd122c0f1d844987e8
Dave Shawley <daveshawley@gmail.com>
2015-07-04 11:24:10 -0400

    Add some documentation.

---
+++ docs/api.rst
@@ -0,0 +1,17 @@
+Python API
+==========
+
+Standard Library Interface
+--------------------------
+.. automodule:: magician.amqp
+   :members:
+
+Off-the-wire Decoding Mechanisms
+--------------------------------
+.. automodule:: magician.wire
+   :members:
+
+Detected Errors
+---------------
+.. automodule:: magician.errors
+   :members:
---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import alabaster
+
+from magician import __version__, version_info
+
+
+project = 'magician'
+copyright = '2015, Dave Shawley'
+version = __version__
+release = '.'.join(str(x) for x in version_info[:2])
+
+needs_sphinx = '1.0'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'alabaster',
+]
+templates_path = []
+source_suffix = '.rst'
+source_encoding = 'utf-8-sig'
+master_doc = 'index'
+pygments_style = 'sphinx'
+html_theme = 'alabaster'
+html_theme_path = [alabaster.get_path()]
+html_static_path = []
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'searchbox.html',
+    ],
+}
+html_theme_options = {
+    'github_user': 'dave-shawley',
+    'github_repo': 'magician',
+    'description': 'Pulling rabbits out of an async hat',
+    'github_banner': True,
+}
+exclude_patterns = []
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/3/', None),
+}
